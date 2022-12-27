b70a8cb4c25c51a140fb2c05f9803fe35911f317
Tech Support Ender 2 Pro <118843387+GTPGllc@users.noreply.github.com>
2022-12-27 14:33:15 -0500

    Add files via upload

---
+++ Configuration Files/Version.h
@@ -0,0 +1,40 @@
+// Release Version: v4.2.3
+// Release Date: 12-27-2022
+/**
+ * Firmware Version 4.2.3
+ * Only For CPU Type: STM32F103RET6 (For older Creality v4.2.3 Ender 2 Pro Motherboards)
+
+
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 Marlin Firmware
+ * CHANGE DATE 12-27-2022
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ * 
+ * For The Creality Ender 2 Pro, Version 4.2.3 Firmware is for the v4.2.3 MAIN CONTROL BOARD (CPU Type: STM32F103RET6)
+ * Reconfigured from Original Creality Firmware v2.0.6.1 to v4.2.3 by The 300lb Gorilla
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details................
+ *
+ */
+
+
+#pragma once
+
+#define SHORT_BUILD_VERSION "v4.2.3"  //<---------------------------------Changed for New Version Firmware, now Ender 2 Pro-v4.2.3 (The 300lb Gorilla)
+//#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+#define STRING_DISTRIBUTION_DATE "12-27-2022"  
+#define MACHINE_NAME "Ender 2 Pro v4.2.3"   //<---------------------------Changed for New Version Firmware, now Ender 2 Pro-v4.2.3 (The 300lb Gorilla)
+#define SOURCE_CODE_URL "github.com/GTPGllc"
+//#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+#define WEBSITE_URL "github.com/GTPGllc"  //<-----------------------------Changed for New Version Firmware, now Ender 2 Pro-v4.2.3 (The 300lb Gorilla)
+//#define USB_DEVICE_VENDOR_ID           0x0000
+//#define USB_DEVICE_PRODUCT_ID          0x0000
+//#define USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
+
+
